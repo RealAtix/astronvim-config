25a83e9dd6da422146e3ee403a502b9e59bc9b3c
Raz <raz@raztech.io>
2023-04-13 22:56:23 +0200

    AstroNvim v3 config

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,9 +1,61 @@
 return {
-  -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
-  -- example of imporing a plugin, comment out to use it or add your own
-  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  -- Language packs
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.java", enabled = false }, -- Not worth missing out on IntelliJ, required JDK-17+
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.yaml" },
+
+  -- Bars and lines
+  { import = "astrocommunity.bars-and-lines.heirline-mode-text-statusline" },
+  -- {
+  --   "rebelot/heirline.nvim",
+  --   opts = function(_, opts)
+  --     local status = require "astronvim.utils.status"
+  --
+  --     opts.statusline = {
+  --       -- statusline
+  --       hl = { fg = "fg", bg = "bg" },
+  --       status.component.mode { mode_text = { padding = { left = 1, right = 1 } } }, -- add the mode text
+  --       status.component.git_branch(),
+  --       status.component.file_info { filetype = {}, filename = false, file_modified = false },
+  --       status.component.git_diff(),
+  --       status.component.diagnostics(),
+  --       status.component.fill(),
+  --       status.component.cmd_info(),
+  --       status.component.fill(),
+  --       status.component.lsp(),
+  --       status.component.treesitter(),
+  --       status.component.nav(),
+  --       -- remove the 2nd mode indicator on the right
+  --     }
+  --
+  --     return opts
+  --   end,
+  -- },
+
+  -- Utils
+  { import = "astrocommunity.core-runner.sniprun", enabled = false },
+  { import = "astrocommunity.completion.copilot-lua-cmp", enabled = false },
+  { import = "astrocommunity.editing-support.todo-comments-nvim" },
+  {
+    "todo-comments.nvim",
+    keys = {
+      { "<leader>T", "<cmd>TodoTelescope<cr>", desc = "Telescope TODOs" },
+    },
+  },
+  { import = "astrocommunity.motion.leap-nvim" },
+
+  -- Colorschemes
+  { import = "astrocommunity.colorscheme.nightfox", enabled = true },
+  { import = "astrocommunity.colorscheme.kanagawa", enabled = true },
+  { import = "astrocommunity.colorscheme.rose-pine" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.tokyonight" },
 }
