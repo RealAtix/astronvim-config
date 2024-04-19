60bd926ce77d885e3b3f7970d5d8f5ed1b46d011
Raz <raz@raztech.io>
2024-04-19 12:13:20 +0200

    Migrate community plugins to v4

--- lua/community.lua
+++ lua/community.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
@@ -8,5 +8,51 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
+
+  -- Language packs
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.vue" },
+  { import = "astrocommunity.pack.java", enabled = true }, -- Not worth missing out on IntelliJ, requires JDK-17+
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.terraform" },
+  { import = "astrocommunity.pack.html-css" },
+
+  -- Bars and lines
+  { import = "astrocommunity.recipes.heirline-mode-text-statusline" },
+
+  -- Utils
+  { import = "astrocommunity.core-runner.sniprun", enabled = false },
+  { import = "astrocommunity.completion.copilot-lua-cmp", enabled = false },
+  { import = "astrocommunity.editing-support.todo-comments-nvim" },
+  {
+    "todo-comments.nvim",
+    keys = {
+      { "<leader>T", "<cmd>TodoTelescope<cr>", desc = "Telescope TODOs" },
+    },
+  },
+  -- { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
+
+  -- Motion
+  { import = "astrocommunity.motion.leap-nvim" },
+  { import = "astrocommunity.motion.nvim-surround", enabled = false },
+  { import = "astrocommunity.motion.harpoon" },
+
+  -- Colorschemes
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+  { import = "astrocommunity.colorscheme.nightfox-nvim", enabled = true },
+  { import = "astrocommunity.colorscheme.kanagawa-nvim", enabled = true },
+  { import = "astrocommunity.colorscheme.catppuccin", enabled = false },
+  {
+    "nyoom-engineering/oxocarbon.nvim",
+  },
+  {
+    "marko-cerovac/material.nvim",
+    init = function() vim.g.material_style = "deep ocean" end,
+  },
 }
